f459a3b999461814c0ba11ab9834662828496283
vvansuit <vvansuit@outlook.com>
2025-01-09 18:32:53 -0300

    feat: skeleton home page

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+const Home = () => {
+  return (
+    <section className = "w-full flex-center flex-col">
+        <h1 className = "head_text text-center">
+            Discovery and Share
+            <br className = "max-md:hidden" />
+        </h1>
+        <span className = "orange_gradient text-center">AI-Powered Prompts</span> 
+        <p className = "desc text-center">
+            The Open Source Tool for sharing your prompts
+            with the whole WORLD 🌎
+        </p>
+    </section>
+  )
+}
+
+export default Home
